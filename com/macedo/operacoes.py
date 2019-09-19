d5f8ea1e48d8c52f65ded9fccc31de505f7f918f
Lucas Macedo Canela <1901014@SALAS.AULAS>
2019-09-19 20:37:59 -0300

    Programação e testes

---
+++ com/macedo/operacoes.py
@@ -0,0 +1,9 @@
+class Operacoes():
+	
+	def multiplicacaoPor3(self,valor):
+		resultado = valor * 3
+		return resultado
+		
+	def multiplicacaoPor7(self,valor):
+		resultado = valor * 7
+		return resultado
---
+++ com/macedo/test/test_operacoes.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from com.macedo.operacoes import Operacoes
+
+class TestOperacoes(TestCase):
+
+	def setUp(self):
+		self.operacoes = Operacoes()
+		
+	def test_Por3(self):
+		self.assertEqual(self.operacoes.multiplicacaoPor3(5), 15)
+		
+	def test_Por7(self):
+		self.assertEqual(self.operacoes.multiplicacaoPor7(4), 28)
